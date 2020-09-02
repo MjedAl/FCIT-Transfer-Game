b4c066bb94f679e927470e6599db4af3e8d668df
MjedAl <44271342+MjedAl@users.noreply.github.com>
2020-09-02 07:16:15 +0300

    update

--- PYTHON part/main.py
+++ PYTHON part/main.py
@@ -1,19 +1,6 @@
-# PLEASE READ FIRST
-# Hi. what i made was a game where player have to move around and reach the end goal based on his grades
-# if a player pass the game it means that he can transfer so in that case we will check if the transfer is avaialbe this semester by reading the status from a database
-# if's open we will add his grades to the DB ( it's better to take it from ODUS to validate it but since i dont have access i'll ask the user to put it)
-# there's a web-page where you can log in and see the students who are edible for the transfer and open or close the transfer
-# https://www.a-fa.sa/FCIT_Transfer/
-# Email: 1@mjed.xyz
-# Password: 123321 (i know it's bad password, it's just for demo)
-# With that being said i have also two notes.
-# 1- the code might not run as a .py because i used multiple libraries and edited in their code so it's better to see the .exe
-# 2- i have tried using arabic with printing other than the usual issues with LTR and RTL it's completly not supported in the windows console (when opening it from .exe)
-#    so that's why i'll be printing in english.
+# PLEASE SEE README.txt FIRST
+# Author: Abdulmajeed alahmadi
 
-# Name: Abdulmajeed Fauzi alahmadi
-# ID: -
-# Thanks
 
 
 import pygame as pg
